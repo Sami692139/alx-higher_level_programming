17de0e8669944dfc6dafe4147ace4454e2c7fd5a
Sami692139 <samuel.tassew2020@gmail.com>
2022-09-28 22:32:50 +0300

    sami

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
-
 def best_score(a_dictionary):
-        if a_dictionary is None:
-                return None
-        best = 0
-        best_key = None
-        for key, val in a_dictionary.items():
-                if val > best:
-                        best = val
-                        best_key = key
-        return best_key
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
